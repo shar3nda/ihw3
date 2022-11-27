cd48583072af86c381a00e257ab6b4f59fac9a36
shar3nda <vyavya2003@yandex.ru>
2022-11-27 21:27:45 +0300

    implement C solution

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
-#include <stdio.h>
 #include "math.h"
+#include "stdio.h"
+
+double cth(double x) {
+    if (x < 0) {
+        return cth(-x);
+    }
+
+    double ans = 1;
+    double prev = 1, cur = 1;
+    double i = 1;
+
+    do {
+        prev = cur;
+        cur = 2 * exp(-2 * i * x);
+        if (fabs(cur - prev) < 0.001) {
+            break;
+        }
+        ans += cur;
+    } while (++i);
+
+    return ans;
+}
+
+
 
 int main() {
-    printf("%f", cosh(0.5));
-
+    printf("my cth=%f, math.h cth=%f", cth(1.1), 1.0/tanh(1.1));
     return 0;
 }
